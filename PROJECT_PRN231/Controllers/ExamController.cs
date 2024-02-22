52c900afba2e476ebda6f2d67d354e63ff8ef252
swtteam5 <nguyenminhkk2002@gmail.com>
2024-02-22 14:28:28 +0700

    demo repository pattern

--- PROJECT_PRN231/Controllers/ExamController.cs
+++ PROJECT_PRN231/Controllers/ExamController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PROJECT_PRN231.Interface;
+using PROJECT_PRN231.Models;
+using PROJECT_PRN231.Models.ViewModel;
 
 namespace PROJECT_PRN231.Controllers
 {
-    public class ExamController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
     {
-        public IActionResult Index()
+        public IExamRepository _ExamRepository;
+        public ExamController(IExamRepository examRepository)
         {
-            return View();
+            _ExamRepository = examRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_ExamRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("id")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var find = _ExamRepository.GetById(id);
+                if (find == null)
+                {
+                    return NotFound();
+                }
+                return Ok(find);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        public IActionResult Create(ExamVM exam)
+        {
+            try
+            {
+                return Ok(_ExamRepository.Create(exam));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("id")]
+        public ActionResult Update(int id, Exam exam)
+        {
+            if (id != exam.ExamId)
+            {
+                return NotFound();
+            }
+            try
+            {
+                _ExamRepository.Update(exam);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _ExamRepository.Delete(id);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ PROJECT_PRN231/Interface/IExamRepository.cs
@@ -0,0 +1,14 @@
+﻿using PROJECT_PRN231.Models;
+using PROJECT_PRN231.Models.ViewModel;
+
+namespace PROJECT_PRN231.Interface
+{
+    public interface IExamRepository
+    {
+        List<Exam> GetAll();
+        Exam GetById(int id);
+        Exam Create(ExamVM examVM);
+        void Update(Exam examVM);
+        void Delete(int id);
+    }
+}
