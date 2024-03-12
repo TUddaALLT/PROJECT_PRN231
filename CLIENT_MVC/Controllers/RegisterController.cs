29a204c5f206c8f8bf3223751a1bc997cca6458d
swtteam5 <nguyenminhkk2002@gmail.com>
2024-03-12 21:41:00 +0700

    update FE

---
+++ CLIENT_MVC/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using CLIENT_MVC.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PROJECT_PRN231.Models;
+
+namespace CLIENT_MVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        const string ACCESS_CONTROLLER = "Auth/";
+        private readonly APIHelper _apiHelper;
+        public RegisterController(APIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _apiHelper.RequestPostAsync<Register>(ACCESS_CONTROLLER + "Register", register);
+                return RedirectToAction("Index","Home");
+            }
+            return View(register);
+        }
+    }
+}
---
+++ CLIENT_MVC/Utilities/APIHelper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace CLIENT_MVC.Utilities
+{
+    public class APIHelper
+    {
+        const string BaseUrl = "https://localhost:8080/api/";
+
+        public async Task<IActionResult> RequestGetAsync<T>(string path)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(BaseUrl + path);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody =  await response.Content.ReadAsStringAsync();
+                    var responseData = JsonConvert.DeserializeObject<T>(responseBody);
+                    return new OkObjectResult(responseData);
+                }
+                else
+                {
+                    throw new Exception("Get api failed");
+                }
+            }
+        }
+
+        public async Task<IActionResult> RequestPostAsync<T>(string path, T body)
+        {
+            using (var client = new HttpClient())
+            {
+                string jsonContent = JsonConvert.SerializeObject(body);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(BaseUrl + path, content);
+                if(response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var responseData = JsonConvert.DeserializeObject<T>(responseBody);
+                    return new OkObjectResult("Register successfull");
+                }
+                else if (response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    return new BadRequestObjectResult("Bad request");
+                }
+                else
+                {
+                    return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+            }
+        }
+    }
+}
